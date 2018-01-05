096b4233e69f17fb4e7a2272efcdd14c7424694e
Leonardo Rosa <leosrosa96@gmail.com>
2018-01-05 11:11:51 -0200

    first commit

---
+++ Funcional/BatalhaNaval/Program.fs
@@ -0,0 +1,154 @@
+﻿open System
+
+// Funções para o tabuleiro
+
+let randomizer = System.Random()
+
+let rec insert_object (board : list<int * int * char>) (cobject : char) (start_pos : int * int) (length : int) (direction : int) =
+    if length > 0 then
+        let new_board = (fst start_pos, snd start_pos, cobject) :: board in
+            let new_length = length - 1 in
+                match direction with
+                    | 0 -> let new_posx = fst start_pos + 1 in
+                            insert_object new_board cobject (new_posx, snd start_pos) new_length direction
+                    | 1 -> let new_posy = snd start_pos + 1 in
+                            insert_object new_board cobject (fst start_pos, new_posy) new_length direction
+    else
+        board
+
+
+let rec get_object (board : list<int * int * char>) (pos : int * int)  =
+    if board.IsEmpty then
+        '0'
+    else
+        let board_pos = board.Head in
+            let posx, posy, cobject = board_pos in
+                if fst pos = posx && snd pos = posy then
+                    cobject
+                else
+                    get_object board.Tail pos
+
+
+let rec is_empty (board : list<int * int * char>) (start_pos : int * int) (length : int) (direction : int) =
+    if length > 0 then
+        let cobject = get_object board start_pos in
+            match cobject with
+                | '0' -> let new_length = length - 1 in
+                            match direction with
+                                | 0 -> let new_posx = fst start_pos + 1 in
+                                        is_empty board (new_posx, snd start_pos) new_length direction
+                                | 1 -> let new_posy = snd start_pos + 1 in
+                                        is_empty board (fst start_pos, new_posy) new_length direction
+                | _ -> false
+    else
+        true
+
+let rec create_object (board : list<int * int * char>) (cobject : char) (length : int) =
+    let start_pos = (randomizer.Next(0, 16 - length), randomizer.Next(0, 16 - length)) in
+        let direction = randomizer.Next(0, 2) in
+            if is_empty board start_pos length direction then
+                insert_object board cobject start_pos length direction
+            else
+                create_object board cobject length
+
+
+// Funções para leitura de input
+
+let rec read_coord_x() =
+
+    printf "Digite a coordenada X:"
+    
+    let user_input = Console.ReadLine() in
+        match System.Int32.TryParse user_input with
+            | true, v -> let coordx = v in
+                            if coordx >= 0 && coordx <= 14 then
+                                coordx
+                            else
+                                read_coord_x ()
+            | false, _ -> -1
+
+let rec read_coord_y() =
+
+    printf "Digite a coordenada Y:"
+    
+    let user_input = Console.ReadLine() in
+        match System.Int32.TryParse user_input with
+            | true, v -> let coordy = v in
+                            if coordy >= 0 && coordy <= 14 then
+                                coordy
+                            else
+                                read_coord_y ()
+            | false, _ -> -1
+            
+let get_user_input() =
+    
+    let coordx = read_coord_x () in
+        match coordx with
+           | -1 -> (-1, -1)
+           | _ -> let coordy = read_coord_y () in
+                        match coordy with
+                            | -1 -> (-1, -1)
+                            | _ -> (coordx, coordy)
+
+
+// Funções de controle do jogo
+
+let print_object (cobject : char) =
+
+    match cobject with
+        | '0' -> printf "Nothing was hit\n"
+        | 's' -> printf "You hit a submarine\n"
+        | 'n' -> printf "You hit a ship\n"
+        | 'm' -> printf "You hit a mine\n"
+
+let rec remove_object (board : list<int * int * char>) (pos : int * int) =
+
+    if board.IsEmpty then
+        []
+    else
+        let board_pos = board.Head in
+            let posx, posy, _ = board_pos in
+                if fst pos = posx && snd pos = posy then
+                    board.Tail
+                else
+                    board.Head :: remove_object board.Tail pos
+
+let rec play_game (board : list<int * int * char>) =
+
+    let coords = get_user_input() in
+        match coords with
+            | (-1, -1) -> printf "Game ended"
+            | _ -> let cobject = get_object board coords in
+                        let _ = print_object cobject in
+                            match cobject with
+                                | '0' -> play_game board
+                                | _ ->  let new_board = remove_object board coords in                                        
+                                            if new_board.IsEmpty then
+                                                printf "You won!"
+                                            else
+                                                play_game new_board
+                    
+
+// Criação do tabuleiro
+let submarino1 = create_object [] 's' 2
+let submarino2 = create_object submarino1 's' 2
+let submarino3 = create_object submarino2 's' 2
+let submarino4 = create_object submarino3 's' 2
+
+let navio1 = create_object submarino4 'n' 3
+let navio2 = create_object navio1 'n' 3
+let navio3 = create_object navio2 'n' 3
+
+let mina1 = create_object navio3 'm' 1
+let mina2 = create_object mina1 'm' 1
+let mina3 = create_object mina2 'm' 1
+let mina4 = create_object mina3 'm' 1
+let mina5 = create_object mina4 'm' 1
+
+let board = mina5
+
+// Fim da criação do tabuleiro
+
+printf "%A\n" board
+
+play_game board
---
+++ OO/BatalhaNaval/Program.fs
@@ -0,0 +1,179 @@
+﻿open System.Collections.Generic
+open System
+
+let randomizer = System.Random()
+
+type Point(x : int, y : int) = 
+
+    let posx = x
+    let posy = y
+
+    member this.getX() =
+        posx
+
+    member this.getY() =
+        posy
+
+    override a.Equals(bobj) =
+        match bobj with
+        | :? Point as b -> a.getX() = b.getX() && a.getY() = b.getY()
+        | _ -> false
+
+    member this.teste() =
+        printf "%d, %d\n" posx posy
+
+[<AbstractClass>]
+type Object(cobject : char, position : List<Point>) =
+
+    let Cobject = cobject
+    let PositionList = position
+
+    member this.hasPoint(pos : Point) = 
+        PositionList.Contains(pos)
+
+    member this.removePoint(pos : Point) =
+        ignore (PositionList.Remove(pos))
+        PositionList.Count = 0
+
+    abstract member getName : unit -> string
+
+    member this.teste() =
+        PositionList.ForEach(fun elem ->
+            elem.teste())
+
+type Mine(position : List<Point>) = 
+    inherit Object('m', position)
+
+    override this.getName() = "mine"
+
+type Submarine(position : List<Point>) =
+    inherit Object('s', position)
+
+    override this.getName() = "submarine"
+
+type Ship(position : List<Point>) =
+    inherit Object('n', position)
+
+    override this.getName() = "ship"
+
+
+type Board(objects : List<Object>) = 
+    
+    let Objects = objects
+
+    let generateRandomPosition(length : int, direction : int) =
+        let pointList = new List<Point>()
+        let startPositionStable = randomizer.Next(0, 16 - length)
+        let startPositionIncrementable = randomizer.Next(0, 16 - length)
+        for i = 1 to length do
+            let newPositionIncrementable = startPositionIncrementable + i - 1
+            if direction = 0 then
+                pointList.Add(new Point(newPositionIncrementable, startPositionStable))
+            else
+                pointList.Add(new Point(startPositionStable, newPositionIncrementable))
+        pointList            
+
+    let generateRandomDirection() = 
+        randomizer.Next(0, 2)
+
+    let doesPositionExists(position : Point) =
+        Objects.Exists(fun (elem : Object) ->
+            elem.hasPoint(position)
+        )
+    
+    let doPositionsExist(positions : List<Point>) =
+        positions.Exists(fun (elem : Point) ->
+            doesPositionExists(elem)
+        )
+        
+    member this.createMine() = 
+        let mutable positionOnBoard = generateRandomPosition(1, generateRandomDirection())
+        while doPositionsExist positionOnBoard do
+            positionOnBoard <- generateRandomPosition(1, generateRandomDirection())
+            
+        Objects.Add(new Mine(positionOnBoard))
+
+    member this.createSubmarine() = 
+        let mutable positionOnBoard = generateRandomPosition(2, generateRandomDirection())
+        while doPositionsExist positionOnBoard do
+            positionOnBoard <- generateRandomPosition(2, generateRandomDirection())
+            
+        Objects.Add(new Submarine(positionOnBoard))
+
+    member this.createShip() = 
+        let mutable positionOnBoard = generateRandomPosition(3, generateRandomDirection())
+        while doPositionsExist positionOnBoard do
+            positionOnBoard <- generateRandomPosition(3, generateRandomDirection())
+            
+        Objects.Add(new Ship(positionOnBoard))
+
+    member this.tryHit(position : Point) = 
+        try 
+            let object = Objects.Find(fun (elem : Object) ->
+                elem.hasPoint(position))
+
+            printf "You hit a %s\n" (object.getName())
+
+            if object.removePoint(position) then
+                ignore (Objects.Remove(object))
+            
+            Objects.Count = 0
+        with
+            | :? NullReferenceException -> 
+                printf "Nothing was hit\n"
+                false
+
+    member this.teste() =
+       Objects.ForEach(fun elem ->
+            elem.teste())
+
+    new() =
+        Board(new List<Object>())
+
+let rec readUserInput() =
+    printf "Digite a coordenada X:"
+    let resultX, coordX = System.Int32.TryParse (Console.ReadLine())
+    if resultX then
+        printf "Digite a coordenada Y:"
+        let resultY, coordY = System.Int32.TryParse (Console.ReadLine())
+        if resultY then
+            if coordX >= 0 && coordX <= 14 && coordY >= 0 && coordY <= 14 then
+                (coordX, coordY)
+            else
+                printf "Coordenadas devem estar entre 0 e 14\n"
+                readUserInput()
+        else
+            (-1, -1)
+    else
+        (-1, -1)
+
+
+let playGame(board : Board) = 
+    let mutable coords = readUserInput()
+    while not (coords = (-1, -1)) do
+        if (board.tryHit(new Point(coords))) then
+            printf "You won!"
+            coords <- (-1, -1)
+        else
+            coords <- readUserInput()
+        
+        
+let Board = new Board()
+
+Board.createMine()
+Board.createMine()
+Board.createMine()
+Board.createMine()
+Board.createMine()
+
+Board.createSubmarine()
+Board.createSubmarine()
+Board.createSubmarine()
+Board.createSubmarine()
+
+Board.createShip()
+Board.createShip()
+Board.createShip()
+
+Board.teste()
+playGame(Board)
